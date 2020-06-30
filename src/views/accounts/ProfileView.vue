ab2860d87a7787c39414d32a8f903836f9a70524
scl2589 <chaelinshin96@gmail.com>
2020-07-01 02:09:27 +0900

    Add ProfileView file

---
+++ src/views/accounts/ProfileView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="header">
+      
+    </div>
+    <div class="content">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+}
+</script>
+
+<style>
+
+</style>
