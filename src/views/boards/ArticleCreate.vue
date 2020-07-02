ba721a2aa907957e692667763b9061ae2f693abf
scl2589 <chaelinshin96@gmail.com>
2020-07-02 18:47:46 +0900

    Made Login View Style into scoped and Update Article Create design(raw)

--- src/views/boards/ArticleCreate.vue
+++ src/views/boards/ArticleCreate.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container my-3">
-    <h1>새 글 작성 Test</h1>
-    <hr>
-    <div class="dropdown d-flex justify-content-center">
+    <v-app>
+    <div class="dropdown d-flex">
+      <h5 v-if="selectedBoard" class="mx-2">{{ selectedBoard }}</h5>
       <button class="btn btn-secondary dropdown-toggle mx-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         게시판
       </button>
@@ -10,15 +10,39 @@
         <a class="dropdown-item" @click="selectBoard('ssafy')">싸피 게시판</a>
         <a class="dropdown-item" @click="selectBoard('free')">자유 게시판</a>
       </div>
-      <h5 class="mx-2">{{ selectedBoard }}</h5>
     </div>
+      
+    <v-container id="dropdown-example">
+      <v-row>
+        <v-col cols="12" sm="3">
+          <v-overflow-btn
+            class="my-2"
+            :items="dropdownBoard"
+            label="게시판을 선택해주세요."
+            target="#dropdown-example"
+            v-model="currentBoard"
+          ></v-overflow-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- <v-container>
+      <v-col class="d-flex" cols="1" sm="3">
+        <v-select
+          :items="dropdownBoard"
+          label="Standard"
+        ></v-select>
+      </v-col>
+    </v-container>
+       -->
+
     <div class="title">
       <input v-model="articleCreateData.body.title" id="title" class="w-100" type="text" placeholder="제목을 입력해주세요 :)">
     </div>
     <div class="editor-page my-3">
       <div id="summernote"></div>
     </div>
-    <button type="button" class="btn btn-primary" @click="articleCreateSave">Save</button>
+    <button type="button" class="btn btn-primary" @click="articleCreateSave">작성하기</button>
+  </v-app>
   </div>
 </template>
 
@@ -28,14 +52,17 @@ export default {
   name: 'ArticleCreate',
   data() {
     return {
+      dropdownBoard: ['싸피게시판', '자유게시판'],
+      // currentBoard: null,
       selectedBoard: null,
       articleCreateData: {
         boardName: null,
         body: {
           title: null,
           content: null
-        }
-      }
+        },
+      },
+      
     }
   },
   methods: {
@@ -62,5 +89,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.btn {
+  color: black;
+  background-color: lightpink;
+}
 </style>
