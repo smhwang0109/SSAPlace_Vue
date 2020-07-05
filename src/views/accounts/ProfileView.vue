e9ec0987d37edc4abdc2444a2117ed8d1c9dceca
Soomin Hwang <smhwang0109@gmail.com>
2020-07-05 19:05:49 +0900

    Add ProfileView

--- src/views/accounts/ProfileView.vue
+++ src/views/accounts/ProfileView.vue
@@ -6,46 +6,6 @@
       </div>
       {{profile}}
       
-    </div>
-    <hr>
-    <div class="content">
-      <!-- 본인이 소속된 팀 -->
-      <div class="d-flex justify-content-between">
-        <h5 class="d-inline">우리 팀</h5>
-        <router-link :to="{ name: 'TeamCreate' }" class="btn">팀 생성</router-link>
-      </div>
-
-      <div class="container-fluid mt-4">
-        <div class="row">
-            <div class="col-4 h-30 p-2" v-for="team in teams" :key="team.id">
-              <router-link :to="{ name: 'TeamDetail', params: { teamId: team.id } }" class="card h-100" >
-              <img src="https://picsum.photos/seed/picsum/400/120" class="card-img-top" alt="image" >
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">{{ team.name }}</h5>
-                <span>by {{ team.leader.username }}</span>
-                <p></p>
-                <h5 class="card-subtitle mb-3">{{ team.oneline_description }}</h5>
-                <p class="card-text">
-                  <span v-for="interest_id in team.interests" :key="interest_id"> {{ interests[interest_id-1]["interest"] }} /</span>
-                  <span v-for="language_id in team.front_language" :key="language_id"> {{ languages[language_id-1]["language"] }} /</span>
-                  <span v-for="language_id in team.back_language" :key="language_id"> {{ languages[language_id-1]["language"] }} /</span>
-                  <!-- <br> 현재 구성원 {{ team.current_members }}명 / {{ team.residence }} / ~{{ team.created_at.slice(0,10) }} -->
-                </p>
-                <p class="mt-auto">
-                  현재 팀원 {{ team.current_members }}명 / {{ team.residence }} / ~{{ team.created_at.slice(0,10) }}
-                </p>
-              </div>
-              <div class="card-footer d-flex justify-content-between">
-                <small class="text-muted">{{ team.created_at }}</small>
-                <i class="far fa-bookmark"></i>
-              </div>
-            </router-link>
-            </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
