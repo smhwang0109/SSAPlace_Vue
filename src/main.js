1ab71b44ca76161fb78f12b4fbc3371e559c4f28
scl2589 <chaelinshin96@gmail.com>
2020-07-12 22:42:20 +0900

    Signup - Fix bug & Home - Update TeamView

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import router from './router'
 import store from './store'
 import VueCookies from 'vue-cookies'
 import vuetify from './plugins/vuetify'
-import io from 'socket.io-client'
-import SERVER from '@/api/drf'
+// import io from 'socket.io-client'
+// import SERVER from '@/api/drf'
 
 Vue.use(VueCookies)
 
-const socket = io(SERVER.URL + '/chat/');
+// const socket = io(SERVER.URL + '/chat/');
 
-Vue.prototype.$socket = socket;
+// Vue.prototype.$socket = socket;
 
 Vue.config.productionTip = false
 
