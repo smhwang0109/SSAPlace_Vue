de4af5f162aeef924935b50b0d71abb885aec662
Soomin Hwang <smhwang0109@gmail.com>
2020-07-13 19:22:41 +0900

    Update DRF Url

--- src/api/drf.js
+++ src/api/drf.js
@@ -1,6 +1,6 @@
 export default {
     // URL: 'http://localhost:8000',
-    URL: 'http://ec2-3-34-179-118.ap-northeast-2.compute.amazonaws.com',
+    URL: 'http://ec2-3-34-91-69.ap-northeast-2.compute.amazonaws.com',
     ROUTES: {
       // rest-auth
       signup: '/rest-auth/signup/',
